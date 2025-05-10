cf3290834980e0e400d733b484ac9809b78402ba
Subeen Park <psb0623@gmail.com>
2025-05-11 07:01:04 +0900

    adfadf

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,41 @@ import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { RootSiblingParent } from "react-native-root-siblings";
 
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  async function registerForPushNotificationsAsync() {
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+  
+      if (finalStatus !== 'granted') {
+        alert('푸시 알림 권한이 필요합니다!');
+        return;
+      }
+  
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log('Expo Push Token:', token);
+      return token;
+    } else {
+      alert('푸시 알림은 실제 기기에서만 작동합니다');
+    }
+  }
+
+  registerForPushNotificationsAsync();
 
   if (!loaded) {
     // Async font loading only occurs in development.
